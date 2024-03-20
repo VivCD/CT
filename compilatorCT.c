73e3f7dcb1e8ad6ce67864096bb6fa9580be9cd0
VivCD <116169204+VivCD@users.noreply.github.com>
2024-03-20 22:41:00 +0200

    id,int,real

--- compilatorCT.c
+++ compilatorCT.c
@@ -47,7 +47,7 @@ Token *lastToken = NULL; // Global variable to store last token
 int line = 1;            // Global variable to track current line number
 
 
-void addTk(int code){
+Token *addTk(int code){
     Token *tk;
     SAFEALLOC(tk,Token)
     tk->code=code;
@@ -85,29 +85,52 @@ void tkerr(const Token *tk,const char *fmt,...){
 int getNextToken(const char *input){
     int state=0,nCh;
     char ch;
-    const char *pStartCh = input;
+    const char *pStartCh;
     Token *tk;
     while(1){ // infinite loop
         ch= *input;
-        
         switch(state){
             case 0 :
-                if (isalpha(ch) || ch == '_') {
-                    pStartCh = input;
-                    input++; // Memorize the beginning of the ID
-                    state = 1;    
-                    printf("%c1",ch);    // Set the new state
-                } else if (ch == '\0') {
-                    return END; // Reached the end of input
+                if(isalpha(ch)||ch=='_'){
+                pStartCh=input; // memorizes the beginning of the ID
+                input++; // consume the character
+                state=1; // set the new state
                 }
+                else if(ch=='='){
+                printf("egal");
+                input++;
+                state=3;
+                }
+                else if(ch==' '||ch=='\r'||ch=='\t'){
+                input++; // consume the character and remains in state 0
+                }
+                else if(ch=='\n'){ // handled separately in order to update the current line
+                line++;
+                input++;
+                }
+                else if(ch==0){ // the end of the input string
+                addTk(END);
+                return END;
+                }
+                else if(isdigit(ch) && ch >= '1' && ch <= '9'){
+                    pStartCh=input; 
+                    input++; 
+                    state=3;
+                }
+                else if(ch=='0'){ // the end of the input string
+                    pStartCh=input; 
+                    input++; 
+                    state=5;
+                }
+                else tkerr(addTk(END),"invalid character");
                 break;
             case 1 :
                 if(isalnum(ch)||ch=='_'){
                     input++;
-                    printf("%c2",ch);
+                    
                 }
                 else {
-                    printf("%c3",ch);
+
                     state=2;
                 }
                 break;
@@ -141,22 +164,122 @@ int getNextToken(const char *input){
                 
             else 
             {
-               
-                printf("%c4",ch);
-                // if no keyword, then it is an ID
-                addTk(ID);
-                Token *tk = lastToken;
-                SAFEALLOC(tk->text, char);
-                tk->text = strndup(pStartCh, nCh);
-                input += nCh;
-                return ID;
-               
                 
-            }  
-           
+                // if no keyword, then it is an ID
+                tk = addTk(ID);
+                tk->text = strndup(pStartCh, nCh);
+                
+            }
+             return tk->code; 
+           case 3:
+               if(isdigit(ch))
+                   input++;
+                else if(ch=='e'||ch=='E'){
+                    input++;
+                    state=10;
+                }
+                else if(ch=='.'){
+                    input++;
+                    state=8;
+                }
+                else 
+                    state=4;
+            break;
+
+            case 4: 
+                tk = addTk(CT_INT);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;   
+
+            case 5:
+                if(isdigit(ch) && ch <= '7')
+                   input++;
+                else if(ch=='8'||ch=='9')
+                    state=3;
+                else if(ch == 'x'){
+                    input++;
+                    state=6;
+                }
+                else if(ch=='e'||ch=='E'){
+                    input++;
+                    state=10;
+                }
+                else 
+                    state=4;
+             break;
+
+            case 6:
+                if ((ch >= '0' && ch <= '9') || ((ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F'))){
+                    input++;
+                    state=7;
+                }else {
+                    // Handle invalid character after '0x'
+                    tkerr(addTk(END), "invalid hexadecimal digit after '0x'");
+                    return -1; // Return error code
+                }
+            break;
+            case 7:
+                 if ((ch >= '0' && ch <= '9') || ((ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F'))){
+                    input++; 
+                }
+                else {
+
+                    state=4;
+                }
+            break;
+
+            case 8:
+                if(isdigit(ch)){
+                    input++;
+                    state=9;
+                }
+            break;
+
+            case 9:
+                if(isdigit(ch))
+                   input++;
+                else if(ch=='e'||ch=='E'){
+                    input++;
+                    state=10;
+                }
+            break;
+
+            case 10:
+                if(isdigit(ch)){
+                    input++;
+                    state=12;
+                }
+                else if(ch=='-'||ch=='+'){
+                    input++;
+                    state==11;
+                }
+            break;
+
+            case 11:
+                if(isdigit(ch)){
+                    input++;
+                    state=12;
+                }
+            break;
+                
+            case 12:
+                if(isdigit(ch))
+                   input++;
+                else 
+                    state=13;
+            break;
+            case 13:
+                tk = addTk(CT_REAL);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code; 
+
+
+
+                
+                 
+              
         }
     }
-   
 
 }
 
@@ -173,16 +296,15 @@ void printTokens(Token *tokens){
         tokens = tokens->next;
     }
 }
-
 int main(int argc, char *argv[]) 
 {
-    if (argc != 2) 
+    if (argc <2) 
     {
-        printf("Usage: %s <input_file>\n", argv[0]);
+        printf("Usage: %s <input_string>\n", argv[0]);
         return 1;
     }
 
-    FILE *file = fopen(argv[1], "r");
+   /* FILE *file = fopen(argv[1], "r");
     if (!file) 
     {
         printf("Error: Could not open file %s\n", argv[1]);
@@ -198,15 +320,23 @@ int main(int argc, char *argv[])
     input[size] = '\0';
 
     fclose(file);
-
+    */
     int token;
-    while ((token = getNextToken(input))) 
-    {
+    
+    for (int i = 1; i<argc; i++){
+    token = getNextToken(argv[i]);
         switch (token) 
         {
             case ID:
                 printf("Identifier: %s\n", lastToken->text);
                 break;
+            case CT_INT:
+                printf("INT number: %s\n", lastToken->text);
+                break;
+            case CT_REAL:
+                printf("REAL number: %s\n", lastToken->text);
+                break;
+
             default:
                 printf("Token code: %d\n", token);
         }
@@ -214,8 +344,6 @@ int main(int argc, char *argv[])
 
     printTokens(tokens);
 
-    free(input);
+    //free(input);
     return 0;
 }
-
-
