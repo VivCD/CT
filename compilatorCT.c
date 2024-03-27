ac81e2c1b3cce7d7e9572659bd40defb055ed958
VivCD <116169204+VivCD@users.noreply.github.com>
2024-03-27 22:57:20 +0200

    toate staturile ? la unele

--- compilatorCT.c
+++ compilatorCT.c
@@ -16,6 +16,32 @@ enum
     CT_REAL,
     CT_CHAR,
     CT_STRING,
+    SPACE,
+    LINE_COMMENT,
+    COMMENT,
+    COMMA,
+    SEMICOLON,
+    LPAR,
+    RPAR,
+    LBRACKET,
+    RBRACKET,
+    LACC,
+    RACC,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    DOT,
+    AND,
+    OR,
+    NOT,
+    EQUAL,
+    ASSIGN,
+    NOTEQ,
+    LESSQ,
+    LESS,
+    GREATERQ,
+    GREATER,
     RETURN,
     DOUBLE,
     STRUCT,
@@ -27,8 +53,6 @@ enum
     FOR,
     INT,
     IF,
-
-    // Add more token codes as needed
 }; 
 
 typedef struct _Token{
@@ -95,41 +119,126 @@ int getNextToken(const char *input){
                 pStartCh=input; // memorizes the beginning of the ID
                 input++; // consume the character
                 state=1; // set the new state
-                }
-                else if(ch=='='){
-                printf("egal");
-                input++;
-                state=3;
-                }
-                else if(ch==' '||ch=='\r'||ch=='\t'){
-                input++; // consume the character and remains in state 0
-                }
-                else if(ch=='\n'){ // handled separately in order to update the current line
-                line++;
-                input++;
-                }
-                else if(ch==0){ // the end of the input string
+                }else if(ch==' '||ch=='\r'||ch=='\t'){
+                    pStartCh=input; 
+                    input++; 
+                    state=28;
+                }else if(ch=='\n'){ // handled separately in order to update the current line
+                    line++;
+                    pStartCh=input; 
+                    input++; 
+                    state=28;
+                }else if(ch==0){ // the end of the input string
                 addTk(END);
                 return END;
-                }
-                else if(isdigit(ch) && ch >= '1' && ch <= '9'){
+                }else if(isdigit(ch) && ch >= '1' && ch <= '9'){
                     pStartCh=input; 
                     input++; 
                     state=3;
-                }
-                else if(ch=='0'){ // the end of the input string
+                }else if(ch=='0'){ // the end of the input string
                     pStartCh=input; 
                     input++; 
                     state=5;
-                }
-                else if(ch=='\''){
+                }else if(ch=='\''){
                     pStartCh=input;
                     input++;
                     state=14;
-
+                }else if(memcmp(input, "\\", 2) == 0){
+                    pStartCh=input;
+                    input++;
+                    state=19;
+                }else if(ch=='/'){
+                    pStartCh=input;
+                    input++;
+                    state=57;
+                }else if(memcmp(input, "//", 2) == 0){
+                    pStartCh=input;
+                    input+=2;
+                    state=29;
+                }else if(ch=='\''){
+                    pStartCh=input;
+                    input++;
+                    state=31;
+                }else if(ch==';'){
+                    pStartCh=input;
+                    input++;
+                    state=32;
+                }else if(ch=='('){
+                    pStartCh=input;
+                    input++;
+                    state=33;
+                }else if(ch==')'){
+                    pStartCh=input;
+                    input++;
+                    state=34;
+                }else if(ch=='['){
+                    pStartCh=input;
+                    input++;
+                    state=35;
+                }else if(ch==']'){
+                    pStartCh=input;
+                    input++;
+                    state=36;
+                }else if(ch=='{'){
+                    pStartCh=input;
+                    input++;
+                    state=37;
+                }else if(ch=='}'){
+                    pStartCh=input;
+                    input++;
+                    state=38;
+                }else if(ch=='+'){
+                    pStartCh=input;
+                    input++;
+                    state=39;
+                }else if(ch=='-'){
+                    pStartCh=input;
+                    input++;
+                    state=40;
+                }else if(ch=='*'){
+                    pStartCh=input;
+                    input++;
+                    state=41;
+                }else if(ch=='/'){
+                    pStartCh=input;
+                    input++;
+                    state=42;
+                }else if(ch=='.'){
+                    pStartCh=input;
+                    input++;
+                    state=43;
+                }else if(memcmp(input, "&&", 2) == 0){
+                    pStartCh=input;
+                    input+=2;
+                    state=44;
+                }else if(memcmp(input, "||", 2) == 0){
+                    pStartCh=input;
+                    input+=2;
+                    state=45;
+                }else if(ch=='!'){
+                    pStartCh=input;
+                    input++;
+                    state=46;
+                }else if(ch=='='){
+                    pStartCh=input;
+                    input++;
+                    state=47;
+                }else if(memcmp(input, "!=", 2) == 0){
+                    pStartCh=input;
+                    input++;
+                    state=50;
+                }else if(ch=='<'){
+                    pStartCh=input;
+                    input++;
+                    state=51;
+                }else if(ch=='>'){
+                    pStartCh=input;
+                    input++;
+                    state=54;
                 }
                 else tkerr(addTk(END),"invalid character");
-                break;
+            break;
+
             case 1 :
                 if(isalnum(ch)||ch=='_'){
                     input++;
@@ -176,7 +285,8 @@ int getNextToken(const char *input){
                 tk->text = strndup(pStartCh, nCh);
                 
             }
-             return tk->code; 
+            return tk->code; 
+
            case 3:
                if(isdigit(ch))
                    input++;
@@ -228,6 +338,7 @@ int getNextToken(const char *input){
                     return -1; // Return error code
                 }
             break;
+
             case 7:
                  if ((ch >= '0' && ch <= '9') || ((ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F'))){
                     input++; 
@@ -263,7 +374,7 @@ int getNextToken(const char *input){
                 }
                 else if(ch=='-'||ch=='+'){
                     input++;
-                    state==11;
+                    state=11;
                 }
             break;
 
@@ -287,19 +398,25 @@ int getNextToken(const char *input){
             return tk->code; 
 
             case 14:
-                if(ch=='//'){
-                    input++;
+                if(memcmp(input, "//", 2) == 0){
+                    input+=2;
                     state=15;
                 }
-                else if(ch=='^'||ch=='\''||ch=='\\'){
+                else if(ch=='^'||ch=='\''){
                     input++;
+                    state=18;
+                }else if(ch=='\\'){
+                    input+=2;
                     state=18;
                 }
             break;
             
             case 15:
-                if(ch=='a'||ch=='b'||ch=='f'||ch=='m'||ch=='n'||ch=='t'||ch=='v'||ch=='\''||ch=='?'||ch=='"'||ch=='\\'||ch=='0'){
+                if(ch=='a'||ch=='b'||ch=='f'||ch=='m'||ch=='n'||ch=='t'||ch=='v'||ch=='\''||ch=='?'||ch=='"'||ch=='0'){
                     input++;
+                    state=16;
+                }else if(ch=='\\'){
+                    input+=2;
                     state=16;
                 }
             break;
@@ -323,10 +440,260 @@ int getNextToken(const char *input){
                 }
             break;
 
+            case 19:
+                 if(ch=='a'||ch=='b'||ch=='f'||ch=='m'||ch=='n'||ch=='t'||ch=='v'||ch=='\''||ch=='?'||ch=='"'||ch=='0'){
+                    input++;
+                    state=20;
+                }else if(ch=='\\'){
+                    input+=2;
+                    state=20;
+                }else if(ch=='/^'||ch=='"'){
+                    input++;
+                    state=22;
+                }
+            break;
 
-                
-                 
-              
+            case 20:
+                if(ch=='a'||ch=='b'||ch=='f'||ch=='m'||ch=='n'||ch=='t'||ch=='v'||ch=='\''||ch=='?'||ch=='"'||ch=='0'){
+                    input++;
+                }else if(ch=='"'){
+                    input++;
+                    state=21;
+                }
+            break;
+
+            case 21:
+               tk = addTk(CT_STRING);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 22:
+                if(ch=='/^'||ch=='"'){
+                    input++;
+                }else if(ch=='"'){
+                    input++;
+                    state=21;
+                }
+            break;
+
+            case 23:
+                if(ch=='*'){
+                    input++;
+                    state=24;
+                }else if (ch=='/^*'){
+                    input+=2;
+                    state=25;
+                }else{
+                    input++;
+                    state=25;
+                }
+            break;
+
+            case 24:
+                if(ch=='*'){
+                    input++;
+                }else if(ch=='/^'||ch=='/'){
+                    input++;
+                    state=25;
+                }
+            break;
+
+            case 25:
+                if(ch=="*"){
+                    input++;
+                    state=26;
+                }else{
+                    input++;
+                    state=23;
+                }
+            break;
+
+            case 26:
+                if(ch=='*'){
+                    input++;
+                }else if(ch=='/'){
+                    input++;
+                    state=27;
+                }
+            break;
+
+            case 27:
+               tk = addTk(COMMENT);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 28:
+               tk = addTk(SPACE);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 29:
+                if(ch=='/^'||ch=='\n'||ch=='\r'||ch=='\0'){
+                    input++;
+                    
+                }
+                else {
+
+                    state=30;
+                }
+            break;
+
+            case 30:
+                tk = addTk(LINE_COMMENT);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 31:
+                tk = addTk(COMMA);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 32:
+                tk = addTk(SEMICOLON);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+            
+            case 33:
+                tk = addTk(LPAR);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 34:
+                tk = addTk(RPAR);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 35:
+                tk = addTk(LBRACKET);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 36:
+                tk = addTk(RBRACKET);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 37:
+                tk = addTk(LACC);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 38:
+                tk = addTk(RACC);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 39:
+                tk = addTk(ADD);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 40:
+                tk = addTk(SUB);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 41:
+                tk = addTk(MUL);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 42:
+                tk = addTk(DIV);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 43:
+                tk = addTk(DOT);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 44:
+                tk = addTk(AND);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 45:
+                tk = addTk(OR);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 46:
+                tk = addTk(NOT);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 47:
+                if(ch=='='){
+                    state=48;
+                }
+                else{
+                    state=49;
+
+                }
+            break;
+
+            case 48:
+                tk = addTk(EQUAL);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;    
+
+            case 49:
+                tk = addTk(ASSIGN);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 50:
+                tk = addTk(NOTEQ);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 51:
+                if(ch=='='){
+                    state=52;
+                }
+                else{
+                    state=53;
+
+                }
+            break;
+
+            case 52:
+                tk = addTk(LESSQ);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 53:
+                tk = addTk(LESS);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+            
+            case 54:
+                if(ch=='='){
+                    state=55;
+                }
+                else{
+                    state=56;
+
+                }
+            break;
+
+            case 55:
+                tk = addTk(GREATERQ);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;
+
+            case 56:
+                tk = addTk(GREATER);
+                tk->text = strndup(pStartCh, nCh);    
+            return tk->code;  
+
+            case 57:
+                if(ch=='*'){
+                    input++;
+                    state=23;
+                }
+            break;
         }
     }
 
@@ -388,7 +755,84 @@ int main(int argc, char *argv[])
             case CT_CHAR:
                 printf("CHAR: %s\n", lastToken->text);
                 break;
-
+            case BREAK:
+                printf("space: %s\n", lastToken->text);
+                break;
+            case COMMA:
+                printf("COMMA: %s\n", lastToken->text);
+                break;
+             case SEMICOLON:
+                printf("SEMICOLON: %s\n", lastToken->text);
+                break;
+            case LPAR:
+                printf("LPAR: %s\n", lastToken->text);
+                break;    
+            case RPAR:
+                printf("RPAR: %s\n", lastToken->text);
+                break;
+            case LBRACKET:
+                printf("LBRACKET: %s\n", lastToken->text);
+                break;  
+            case RBRACKET:
+                printf("RBRACKET: %s\n", lastToken->text);
+                break;
+            case LACC:
+                printf("LACC: %s\n", lastToken->text);
+                break; 
+            case RACC:
+                printf("RACC: %s\n", lastToken->text);
+                break;
+            case ADD:
+                printf("ADD: %s\n", lastToken->text);
+                break; 
+            case SUB:
+                printf("SUB: %s\n", lastToken->text);
+                break; 
+            case MUL:
+                printf("MUL: %s\n", lastToken->text);
+                break;
+            case DIV:
+                printf("DIV: %s\n", lastToken->text);
+                break;
+            case DOT:
+                printf("DOT: %s\n", lastToken->text);
+                break;
+            case AND:
+                printf("AND: %s\n", lastToken->text);
+                break;
+            case OR:
+                printf("OR: %s\n", lastToken->text);
+                break;
+            case NOT:
+                printf("NOT: %s\n", lastToken->text);
+                break;
+            case NOTEQ:
+                printf("NOTEQ: %s\n", lastToken->text);
+                break;
+            case LINE_COMMENT:
+                printf("LINE_COMMENT: %s\n", lastToken->text);
+                break; 
+            case EQUAL:
+                printf("EQUAL: %s\n", lastToken->text);
+                break;  
+            case ASSIGN:
+                printf("ASSIGN: %s\n", lastToken->text);
+                break;
+            case LESSQ:
+                printf("LESSQ: %s\n", lastToken->text);
+                break;
+            case LESS:
+                printf("LESS: %s\n", lastToken->text);
+                break;
+            case GREATERQ:
+                printf("GREATERQ: %s\n", lastToken->text);
+                break;
+            case GREATER:
+                printf("GREATER: %s\n", lastToken->text);
+                break;
+            case COMMENT:
+                printf("OMMENT: %s\n", lastToken->text);
+                break;                        
             default:
                 printf("Token code: %d\n", token);
         }
